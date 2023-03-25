6498fc15843ab8be0e3191e9e4bd803939d9bbff
kat.italiano@gmail.com <kat.italiano>
2023-03-25 12:45:01 +0800

    Home page and nav fixed and updated

--- mentor_scheduling_tool/src/Components/Nav/Nav.jsx
+++ mentor_scheduling_tool/src/Components/Nav/Nav.jsx
@@ -3,7 +3,10 @@ import { Link } from "react-router-dom";
 function Nav() {
     return (
     <nav>
-    <Link to="/">Home</Link>
+    <Link to="/">Home | </Link>
+    <Link to="/mentors">All Mentors | </Link>
+    <Link to="/mentors/1">Mentor 1 | </Link>
+    <Link to="/mentors/2">Mentor 2</Link>
     </nav>);
 }
 
--- mentor_scheduling_tool/src/Pages/MentorProfilePage.jsx
+++ mentor_scheduling_tool/src/Pages/MentorProfilePage.jsx
@@ -10,12 +10,12 @@ function MentorProfilePage() {
     return (
        <div>
        <h2>{`${mentorData.first_name} ${mentorData.last_name}`}</h2>
-                <h3>{`Email: ${mentorData.email}`}</h3>
-                <h3>{`Phone: ${mentorData.phone}`}</h3>
-                <h3>{`City: ${mentorData.city}`}</h3>
-                <h3>{`Skills: ${mentorData.skills}`}</h3>
-                <h3>{`Will travel: ${mentorData.will_travel}`}</h3>
-                <h3>{`Onboarding: ${mentorData.is_active}`}</h3>
+                <p>{`Email: ${mentorData.email}`}</p>
+                <p>{`Phone: ${mentorData.phone}`}</p>
+                <p>{`City: ${mentorData.city}`}</p>
+                <p>{`Skills: ${mentorData.skills}`}</p>
+                <p>{`Will travel: ${mentorData.will_travel}`}</p>
+                <p>{`Onboarding: ${mentorData.is_active}`}</p>
         </div> 
 
     )
