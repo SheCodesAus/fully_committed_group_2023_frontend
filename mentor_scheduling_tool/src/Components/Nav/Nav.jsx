0db40cafa26609022aefa79410c0523ec27c8787
samjsh <sjaneshearman@gmail.com>
2023-03-26 19:50:38 +0800

    Mentor Create Page
    Added a BASIC template for the Mentor Create Page in my own files that I created:
    1. MentorCreatePage.css
    2. MentorCreatePage.jsx
    
    I was thinking that if we like these as a group, we can copy code over into the EXISTING FILES app.css and app.jsx files for a 'General Template' and can then later edit the css to style every new page differently. For now, these files are going up to GIT and I can merge them into app.css and app.jsx after frontend discussion meeting if we think they are applicable enough.

--- mentor_scheduling_tool/src/Components/Nav/Nav.jsx
+++ mentor_scheduling_tool/src/Components/Nav/Nav.jsx
@@ -1,13 +1,15 @@
 import { Link } from "react-router-dom";
 
 function Nav() {
-    return (
+  return (
     <nav>
-    <Link to="/">Home | </Link>
-    <Link to="/mentors">All Mentors | </Link>
-    <Link to="/mentors/1">Mentor 1 | </Link>
-    <Link to="/mentors/2">Mentor 2</Link>
-    </nav>);
+      <Link to="/">Home | </Link>
+      <Link to="/mentors">All Mentors | </Link>
+      <Link to="/mentors/1">Mentor 1 | </Link>
+      <Link to="/mentors/2">Mentor 2</Link>
+      <Link to="/mentor-creation">Create Mentor</Link>
+    </nav>
+  );
 }
 
 export default Nav;
