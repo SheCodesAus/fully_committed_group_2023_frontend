cae8181c4fb14b2efcdb3fe082140f4d5bcb531c
kat.italiano@gmail.com <kat.italiano>
2023-03-31 20:46:39 +0800

    Mentor profile and list now rendering data from backend

--- mentor_scheduling_tool/src/Pages/MentorListPage.jsx
+++ mentor_scheduling_tool/src/Pages/MentorListPage.jsx
@@ -1,14 +1,24 @@
 import { Link } from "react-router-dom";
-import { allMentors } from "../mentordata";
+import { useState, useEffect } from "react";
 
 function MentorListPage() {
+    const [mentorData, setMentorData] = useState ([])
+
+    useEffect(() => {
+        fetch(`${import.meta.env.VITE_API_URL}mentors`)
+        .then((results) => {return results.json();})
+        .then((data) => {setMentorData(data);
+        });
+    }, []);
 
     return (
         <div>
         {
-        allMentors.map((mentor) => {
+        mentorData.map((mentor) => {
             return <div key ={mentor.id}>
-                <Link to={`/mentors/${mentor.id}`}><h2>{`${mentor.first_name} ${mentor.last_name}`}</h2></Link>
+            <Link to={`/mentors/${mentor.id}`}><h2>{`${mentor.first_name} ${mentor.last_name}`}</h2></Link>
+                
+                <p>{`City: ${mentor.city}`}</p>
                 <p>{`Skills: ${mentor.skills}`}</p>
                 <p>{`Will travel? ${mentor.will_travel}`}</p>
                 <p>{`Onboarding active? ${mentor.is_active}`}</p>
@@ -21,3 +31,25 @@ function MentorListPage() {
 
 export default MentorListPage;
 
+
+// import { allMentors } from "../mentordata";
+
+// function MentorListPage() {
+
+//     return (
+//         <div>
+//         {
+//         allMentors.map((mentor) => {
+//             return <div key ={mentor.id}>
+//                 <Link to={`/mentors/${mentor.id}`}><h2>{`${mentor.first_name} ${mentor.last_name}`}</h2></Link>
+//                 <p>{`Skills: ${mentor.skills}`}</p>
+//                 <p>{`Will travel? ${mentor.will_travel}`}</p>
+//                 <p>{`Onboarding active? ${mentor.is_active}`}</p>
+//                 </div>
+//             })
+//         }
+//         </div> 
+//     );
+// }
+
+// export default MentorListPage;
--- mentor_scheduling_tool/src/Pages/MentorProfilePage.jsx
+++ mentor_scheduling_tool/src/Pages/MentorProfilePage.jsx
@@ -1,47 +1,106 @@
+import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { allMentors } from "../mentordata";
 
 function MentorProfilePage() {
-    const { id } = useParams();
+  const { id } = useParams();
+  const [mentorData, setMentorData] = useState(null);
 
-    const mentorData = allMentors.find(mentor => 
-        mentor.id === id);
+  useEffect(() => {
+    fetch(`${import.meta.env.VITE_API_URL}mentors/${id}`)
+      .then((results) => {
+        return results.json();
+      })
+      .then((data) => {
+        setMentorData(data);
+      });
+  }, [id]);
 
-    return (
+  return (
+    <div>
+      {mentorData ? (
         <div>
-            <div>
-                <p>{`First Name: ${mentorData.first_name}`}</p>  
-                <p>{`Last Name: ${mentorData.last_name}`}</p>
-                <p>{`Email: ${mentorData.email}`}</p>
-                <p>{`Phone: ${mentorData.phone}`}</p>
-                <p>{`City: ${mentorData.city}`}</p>
-            </div>
-            <div>
-                <p>{`Will travel? ${mentorData.will_travel}`}</p>
-                <p>{`Junior mentor? ${mentorData.junior_mentor}`}</p>
-                <p>{`Industry mentor? ${mentorData.industry_mentor}`}</p>
-                <p>{`Lead mentor? ${mentorData.lead_mentor}`}</p>
-                <p>{`She codes alumni? ${mentorData.she_codes_alumni}`}</p>
-            </div>
-            <div>
-                <p>{`Skills: ${mentorData.skills}`}</p>
-            </div>
-            <div>
-                <p>{`Current step: ${mentorData.current_step}`}</p>
-                <p>{`Pay Type: ${mentorData.payment_type}`}</p>
-                <p>{`Onboarding active? ${mentorData.is_active}`}</p>
-            </div>
-            <div>
-                <p>{`Notes: ${mentorData.notes}`}</p>
-            </div>
-            <div>
-                <p>{`Feedback: ${mentorData.feedback}`}</p>
-            </div>
-        </div> 
-
-    )
+          <div>
+            <p>{`First Name: ${mentorData.first_name}`}</p>
+            <p>{`Last Name: ${mentorData.last_name}`}</p>
+            <p>{`Email: ${mentorData.email}`}</p>
+            <p>{`Phone: ${mentorData.phone}`}</p>
+            <p>{`City: ${mentorData.city}`}</p>
+          </div>
+          <div>
+            <p>{`Will travel? ${mentorData.will_travel}`}</p>
+            <p>{`Junior mentor? ${mentorData.junior_mentor}`}</p>
+            <p>{`Industry mentor? ${mentorData.industry_mentor}`}</p>
+            <p>{`Lead mentor? ${mentorData.lead_mentor}`}</p>
+            <p>{`She codes alumni? ${mentorData.she_codes_alumni}`}</p>
+          </div>
+          <div>
+            <p>{`Skills: ${mentorData.skills}`}</p>
+          </div>
+          <div>
+            <p>{`Current step: ${mentorData.current_step}`}</p>
+            <p>{`Pay Type: ${mentorData.payment_type}`}</p>
+            <p>{`Onboarding active? ${mentorData.is_active}`}</p>
+          </div>
+          <div>
+            <p>{`Notes: ${mentorData.notes}`}</p>
+          </div>
+          <div>
+            <p>{`Feedback: ${mentorData.feedback}`}</p>
+          </div>
+        </div>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </div>
+  );
 }
 
 export default MentorProfilePage;
+
+// import { useParams } from "react-router-dom";
+// import { allMentors } from "../mentordata";
+
+// function MentorProfilePage() {
+//     const { id } = useParams();
+
+//     const mentorData = allMentors.find(mentor => 
+//         mentor.id === id);
+
+//     return (
+//         <div>
+//             <div>
+//                 <p>{`First Name: ${mentorData.first_name}`}</p>  
+//                 <p>{`Last Name: ${mentorData.last_name}`}</p>
+//                 <p>{`Email: ${mentorData.email}`}</p>
+//                 <p>{`Phone: ${mentorData.phone}`}</p>
+//                 <p>{`City: ${mentorData.city}`}</p>
+//             </div>
+//             <div>
+//                 <p>{`Will travel? ${mentorData.will_travel}`}</p>
+//                 <p>{`Junior mentor? ${mentorData.junior_mentor}`}</p>
+//                 <p>{`Industry mentor? ${mentorData.industry_mentor}`}</p>
+//                 <p>{`Lead mentor? ${mentorData.lead_mentor}`}</p>
+//                 <p>{`She codes alumni? ${mentorData.she_codes_alumni}`}</p>
+//             </div>
+//             <div>
+//                 <p>{`Skills: ${mentorData.skills}`}</p>
+//             </div>
+//             <div>
+//                 <p>{`Current step: ${mentorData.current_step}`}</p>
+//                 <p>{`Pay Type: ${mentorData.payment_type}`}</p>
+//                 <p>{`Onboarding active? ${mentorData.is_active}`}</p>
+//             </div>
+//             <div>
+//                 <p>{`Notes: ${mentorData.notes}`}</p>
+//             </div>
+//             <div>
+//                 <p>{`Feedback: ${mentorData.feedback}`}</p>
+//             </div>
+//         </div> 
+
+//     )
+// }
+
+// export default MentorProfilePage;
 
 
