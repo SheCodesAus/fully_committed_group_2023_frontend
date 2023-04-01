b0163ef7b046d53892261967bdffe5b05da70c2f
samjsh <sjaneshearman@gmail.com>
2023-04-01 10:38:15 +0800

    Adding more pages to Pages folder
    Have deleted the MentorCreationPage, as this logic has been merged into LoginPage

--- mentor_scheduling_tool/src/Pages/MentorCreationPage.jsx
+++
@@ -1,7 +0,0 @@
-import MentorCreationForm from '../Components/MentorCreationForm/MentorCreationForm.jsx'; 
-
-function MentorCreationPage() {
-    return <MentorCreationForm />
-}
-
-export default MentorCreationPage;
