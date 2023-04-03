d16bb7c10ff1515e2b1683524ce6f3f306df6818
Clahryott <ck.truscott@gmail.com>
2023-04-03 19:28:10 +0800

    edit and submit button components

---
+++ src/Components/SubmitButton/SubmitButton.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import { useNavigate, useOutletContext} from "react-router-dom";
+import "./SubmitButton.css";
+
+import React from 'react';
+
+function SubmitButton(props) {
+  const handleClick = () => {
+    // Perform submit action here
+    console.log('Submit button clicked!');
+  };
+
+  return (
+    <button onClick={handleClick}>
+      Submit
+   </button>  { <button class="submit-button">Submit</button> }
+  );
+}
+
+export default SubmitButton;
