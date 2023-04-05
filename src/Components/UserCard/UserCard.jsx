842f4c32915a6b8efacfa8e001064f3e5e5b92d8
Ms-KL <thekristyleigh@gmail.com>
2023-04-05 11:50:40 +0800

    Logged In User Card CSS placed in component

--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -9,29 +9,29 @@ function UserCard(props) {
     return (
 
         <>
-            <table className="user-card">
+            <table id="user-card">
                 <tbody>
                     <tr>
-                        <td className="label"><strong>USER NAME:</strong></td>
-                        <td className="input">{user.username}</td>
+                        <td className="user-item-label"><strong>USER NAME:</strong></td>
+                        <td className="user-item-description">{user.username}</td>
                     </tr>
                     <tr>
-                        <td className="label"><strong>FIRST NAME:</strong></td>
-                        <td className="input">{user.first_name}</td>
+                        <td className="user-item-label"><strong>FIRST NAME:</strong></td>
+                        <td className="user-item-description">{user.first_name}</td>
                     </tr>
                     <tr>
-                        <td className="label"><strong>LAST NAME:</strong></td>
-                        <td className="input">{user.last_name}</td>
+                        <td className="user-item-label"><strong>LAST NAME:</strong></td>
+                        <td className="user-item-description">{user.last_name}</td>
                     </tr>
                     <tr>
-                        <td className="label"><strong>USER EMAIL:</strong></td>
-                        <td className="input">{user.email}</td>
+                        <td className="user-item-label"><strong>USER EMAIL:</strong></td>
+                        <td className="user-item-description">{user.email}</td>
                     </tr>
                     <tr>
-                        <td className="label"><strong>PERMISSIONS:</strong></td>
-                        <td className="input">Create/edit: {user.is_superuser ? "✓" : "✗"}</td>
-                        <td className="input">Admin Portal: {user.is_staff ? "✓" : "✗"}</td>
-                        <td className="input">Active Employee: {user.is_active ? "✓" : "✗"}</td>
+                        <td className="user-item-label"><strong>PERMISSIONS:</strong></td>
+                        <td className="user-item-description">Create/edit: {user.is_superuser ? "✓" : "✗"}</td>
+                        <td className="user-item-description">Admin Portal: {user.is_staff ? "✓" : "✗"}</td>
+                        <td className="user-item-description">Active Employee: {user.is_active ? "✓" : "✗"}</td>
                     </tr>
                 </tbody>
             </table>
