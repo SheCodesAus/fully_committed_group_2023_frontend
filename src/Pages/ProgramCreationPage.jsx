0fb4be5fa57141bf750823336a2cafcab0a36ca8
Simplicity305 <113986047+Simplicity305@users.noreply.github.com>
2023-04-05 11:40:12 +0800

    Comment out code for program and session creation pages
    - Prevent import error for session and program creation pages, as those forms are not exported yet

--- src/Pages/ProgramCreationPage.jsx
+++ src/Pages/ProgramCreationPage.jsx
@@ -1,10 +1,10 @@
-// (WEN) - ALMOST DONE
-// TODO - change import/return when we change the form names to remove the 'create'
+// // UNCOMMENT ONCE PROGRAM FORM CAN BE EXPORTED
+// // TODO - change import/return when we change the form names to remove the 'create'
 
-import CreateProgramForm from "../Components/CreateProgramForm/CreateProgramForm";
+// import CreateProgramForm from "../Components/CreateProgramForm/CreateProgramForm";
 
-function ProgramCreationPage () {
-  return <CreateProgramForm />
-}
+// function ProgramCreationPage () {
+//   return <CreateProgramForm />
+// }
 
-export default ProgramCreationPage;
+// export default ProgramCreationPage;
