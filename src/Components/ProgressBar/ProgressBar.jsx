5309892151eede4ed52a09ced0c9e5e9128c5f8d
samjsh <sjaneshearman@gmail.com>
2023-04-05 19:16:30 +0800

    merge conflict

--- src/Components/ProgressBar/ProgressBar.jsx
+++ src/Components/ProgressBar/ProgressBar.jsx
@@ -1,1 +1,32 @@
-// (SAM) - TO DO
+const ProgressBar = (props) => {
+  const { bgcolor, completed } = props;
+  const containerStyles = {
+    height: 25,
+    width: "60%",
+    backgroundColor: "#87978D",
+    borderRadius: 50,
+    margin: 20,
+  };
+  const fillerStyles = {
+    height: "100%",
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: "inherit",
+    textAlign: "right",
+  };
+  const labelStyles = {
+    padding: 5,
+    color: "white",
+    fontWeight: "bold",
+    fontSize: ".75rem",
+    // verticalAlign: ''
+  };
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        <span style={labelStyles}>{`${completed}%`}</span>
+      </div>
+    </div>
+  );
+};
+export default ProgressBar;
