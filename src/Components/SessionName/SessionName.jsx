67edf60c863338a768ed3115191609ed470ffdf9
Ms-KL <thekristyleigh@gmail.com>
2023-04-06 05:37:13 +0800

    add program type to session name

--- src/Components/SessionName/SessionName.jsx
+++ src/Components/SessionName/SessionName.jsx
@@ -1,15 +1,18 @@
 // DONE - KAT
+// EDITED BY KRISTY TO REFLECT REQUIRED PROGRAM FIELDS
+
 // TO USE INSERT: <SessionName {...sessionData} {...programData} />
+
 import FormatDate from "../DateTime/FormatDate";
 import FormatTime from "../DateTime/FormatTime";
 
 function SessionName(props) {
-    const {module_type, city, start_date, end_date, ...other} = props;
+    const {module_type, city, start_date, end_date, program_type, ...other} = props;
     const formattedStartDate = FormatDate(start_date);
     const formattedTime = FormatTime(start_date);
 
     return( 
-        `${city} - ${module_type} Session - ${formattedStartDate}, ${formattedTime}`
+        `${city} ${program_type} - ${module_type} Session - ${formattedStartDate}, ${formattedTime}`
     );
 }
 
