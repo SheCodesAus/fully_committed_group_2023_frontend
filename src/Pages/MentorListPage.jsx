a05d488d2484e4e6f094fd4fcda6fc8eb2b6c5e0
Simplicity305 <113986047+Simplicity305@users.noreply.github.com>
2023-04-06 16:48:23 +0800

    Add divs to list pages
    - add div for body of content
    - add div to contain h1 and create

--- src/Pages/MentorListPage.jsx
+++ src/Pages/MentorListPage.jsx
@@ -3,39 +3,42 @@
 import { Link } from "react-router-dom";
 import { useState, useEffect } from "react";
 import MentorCreationForm from "../Components/CreateMentorForm/CreateMentorForm.jsx";
-import EditButton from "../Components/EditButton/EditButton.jsx";
 
 function MentorListPage() {
-    const [mentorData, setMentorData] = useState ([])
+    const [mentorData, setMentorData] = useState([])
 
     useEffect(() => {
         fetch(`${import.meta.env.VITE_API_URL}mentors`)
-        .then((results) => {return results.json();})
-        .then((data) => {setMentorData(data);
-        });
+            .then((results) => { return results.json(); })
+            .then((data) => {
+                setMentorData(data);
+            });
     }, []);
 
     return (
-        <div>
-        <h1>MENTORS</h1>
+        <div className="page-content list">
+            <div className="list-header">
+                <h1>MENTORS</h1>
+                <Link className="create-button" to={`/mentors/create`}>Create</Link>
+            </div>
 
-        <Link className="create-button" to={`/mentors/create`}>Create</Link>
+            <div>
+                {
+                    mentorData.map((mentor) => {
+                        return <div key={mentor.id}>
+                            <Link to={`/mentors/${mentor.id}`}><h2>{`${mentor.first_name} ${mentor.last_name}`}</h2></Link>
 
-        {
-        mentorData.map((mentor) => {
-            return <div key ={mentor.id}>
-            <Link to={`/mentors/${mentor.id}`}><h2>{`${mentor.first_name} ${mentor.last_name}`}</h2></Link>
-                
-                <p>{`City: ${mentor.city}`}</p>
-                <p>{`Skills: ${mentor.skills}`}</p>
-                <p>{`Will travel? ${mentor.will_travel}`}</p>
-                <p>{`Onboarding active? ${mentor.is_active}`}</p>
-                </div>
-            })
-        }
-        </div> 
+                            <p>{`City: ${mentor.city}`}</p>
+                            <p>{`Skills: ${mentor.skills}`}</p>
+                            <p>{`Will travel? ${mentor.will_travel}`}</p>
+                            <p>{`Onboarding active? ${mentor.is_active}`}</p>
+                        </div>
+                    })
+                }
+            </div>
+        </div>
     );
-        
+
 }
 
 export default MentorListPage;
