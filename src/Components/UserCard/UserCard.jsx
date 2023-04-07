7ed704c5a18736ac9ec3c47ff25ea1856ac17e95
Simplicity305 <113986047+Simplicity305@users.noreply.github.com>
2023-04-08 06:39:09 +0800

    Update user card

--- src/Components/UserCard/UserCard.jsx
+++ src/Components/UserCard/UserCard.jsx
@@ -14,25 +14,25 @@ function UserCard(props) {
             <table id="user-card">
                 <tbody>
                     <tr>
-                        <td className="user-item-label"><strong>USER NAME:</strong></td>
+                        <td className="user-item-label"><strong>Username</strong></td>
                         <td className="user-item-description">{user.username}</td>
                     </tr>
                     <tr>
-                        <td className="user-item-label"><strong>FIRST NAME:</strong></td>
+                        <td className="user-item-label"><strong>First Name</strong></td>
                         <td className="user-item-description">{user.first_name}</td>
                     </tr>
                     <tr>
-                        <td className="user-item-label"><strong>LAST NAME:</strong></td>
+                        <td className="user-item-label"><strong>Last Name</strong></td>
                         <td className="user-item-description">{user.last_name}</td>
                     </tr>
                     <tr>
-                        <td className="user-item-label"><strong>USER EMAIL:</strong></td>
+                        <td className="user-item-label"><strong>Email</strong></td>
                         <td className="user-item-description">{user.email}</td>
                     </tr>
                     <tr>
-                        <td className="user-item-label"><strong>PERMISSIONS:</strong></td>
+                        <td className="user-item-label"><strong>Permissions</strong></td>
                         <td className="user-item-description">Create/edit: {user.is_superuser ? "✓" : "✗"}</td>
-                        <td className="user-item-description">Admin Portal: {user.is_staff ? "✓" : "✗"}</td>
+                        <td className="user-item-description">Admin portal: {user.is_staff ? "✓" : "✗"}</td>
                         <td className="user-item-description">Active Employee: {user.is_active ? "✓" : "✗"}</td>
                     </tr>
                 </tbody>
