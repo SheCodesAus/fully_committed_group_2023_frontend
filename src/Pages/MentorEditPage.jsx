31740dd34354b62a88c77dff6112442b1348b99b
Ms-KL <thekristyleigh@gmail.com>
2023-04-09 20:17:10 +0800

    new pages added

---
+++ src/Pages/MentorEditPage.jsx
@@ -0,0 +1,10 @@
+// KRISTY
+
+import EditMentorForm from "../Components/CreateMentorForm/EditMentorForm";
+
+
+function MentorEditPage() {
+  return <EditMentorForm />
+}
+
+export default MentorEditPage;
